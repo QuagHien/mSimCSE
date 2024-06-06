f7622ae936c7dc357abcd07b49e42d2cf9658a38
Quang Hien <130220114+QuagHien@users.noreply.github.com>
2024-06-06 10:35:49 +0700

    Update download_nli.sh

--- data/download_nli.sh
+++ data/download_nli.sh
@@ -1,2 +1,2 @@
 wget https://huggingface.co/datasets/wanhin/msimcse_512_seqlen/resolve/main/msimcse_512_seqlen.csv
-wget https://huggingface.co/datasets/wanhin/msimcse_test_512/resolve/main/msimcse_test_512.csv
+wget https://huggingface.co/datasets/wanhin/msimcse_test_512/resolve/main/msimcse_test.csv
