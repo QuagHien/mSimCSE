e70ab71ff87d0fd9366e36c6928caf61fcc585c6
Quang Hien <130220114+QuagHien@users.noreply.github.com>
2024-06-13 09:43:04 +0700

    Update train_cross.sh

--- train_cross.sh
+++ train_cross.sh
@@ -3,13 +3,14 @@
 # In this example, we show how to train SimCSE on unsupervised Wikipedia data.
 # If you want to train it with multiple GPU cards, see "run_sup_example.sh"
 # about how to use PyTorch's distributed data parallel.
-
+export CUDA_VISIBLE_DEVICES=0
 python train.py \
     --model_name_or_path xlm-roberta-base  \
     --train_file data/msimcse_512_seqlen.csv  \
-    --output_dir result/xlm-roberta-base-cross_vi-en \
-    --num_train_epochs 10 \
-    --per_device_train_batch_size 64 \
+    --output_dir result/xlm-roberta-large-cross_all \
+    --num_train_epochs 20 \
+    --per_device_train_batch_size 10 \
+    --gradient_accumulation_steps 3 \
     --learning_rate 1e-5 \
     --max_seq_length 512 \
     --evaluation_strategy steps \
